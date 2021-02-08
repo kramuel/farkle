b028cd924822d650ffbe352b0bbe8b0a17ad684a
Basma A <basma_812@hotmail.com>
2021-02-08 09:33:26 +0100

    defs

--- farkle/farkle.py
+++ farkle/farkle.py
@@ -1,13 +1,18 @@
 #import random
 
+#welcome_msg
+
+#def play(dice, players):
+
+#def roll_dice(dice, players):
 
 
-#def roll_dice
-
-
-#def reroll
+#def reroll(dice, players):
 
 
 
-#
+#def score(dice, players):
+
+
+#kanske def rounds(players):
 
