b783f2cbcdad3c9cbfd6ba163b47e1d3da6903b1
Basma A <basma_812@hotmail.com>
2021-02-08 09:37:15 +0100

    Revert "defs"
    This reverts commit b028cd924822d650ffbe352b0bbe8b0a17ad684a.

--- farkle/farkle.py
+++ farkle/farkle.py
@@ -1,18 +1,13 @@
 #import random
 
-#welcome_msg
-
-#def play(dice, players):
-
-#def roll_dice(dice, players):
 
 
-#def reroll(dice, players):
+#def roll_dice
+
+
+#def reroll
 
 
 
-#def score(dice, players):
-
-
-#kanske def rounds(players):
+#
 
