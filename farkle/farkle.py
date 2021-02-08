a01ba932b653949a68634cf817b6d97da4e87ad8
Samuel Karlström <sam.karlstrm@gmail.com>
2021-02-08 10:52:13 +0100

    first commit

--- farkle/farkle.py
+++ farkle/farkle.py
@@ -1,50 +1,42 @@
-#import random
- 
+# import random
+print("hello world")
 
-#welcome_msg
-
-
-
-#def play(dice, players):
+# welcome_msg
 
 
-
-#def roll_dice(dice, players):
-
+# def play(dice, players):
 
 
-
-#def reroll(dice, players):
-
+# def roll_dice(dice, players):
 
 
+# def reroll(dice, players):
 
 
-#def score(dice, players):
+# def score(dice, players):
 
-     #   Calculate a Farkle score using the traditional scoring method, which is:
-     #     4 1s:     2,000 points
-     #     3 1s:     1,000 points
-     #     Single 1: 100 points
-     #     Single 5: 50 points
-     #     Triple of any non-1 number:    100 x number showing
-     #     Quadruple of any non-1 number: Double the triple score
-     #     """
-     # Notes:
-     #     - Doubles score nothing (unless a 1 or a 5) as above
-     #     - All scoring dice must be rolled in a single turn (i.e. they
-     #     are not additive over turns)
-     #     - Rolling all 6 will be two sets and scored accordingly
-     #         [4] + [2]
-     #         [3] + [3]
-     #         [2] + [2] + [2]
-     #
-     # Examples:
-     #     1,1,1,5,5,5 ==> 1500 (1000 for 1s + 250 for 3 5s)
-     #     1,1,1,1,6,6 ==> 2000 (2000 for 4 1s)
-     #     5,3,6,5,3,3 ==> 400 (300 for 3 3s + 100 for 2 5s)
-     #     1,2,2,3,3,5 ==> 150 (100 for a 1 and 50 for a 5)
+#   Calculate a Farkle score using the traditional scoring method, which is:
+#     4 1s:     2,000 points
+#     3 1s:     1,000 points
+#     Single 1: 100 points
+#     Single 5: 50 points
+#     Triple of any non-1 number:    100 x number showing
+#     Quadruple of any non-1 number: Double the triple score
+#     """
+# Notes:
+#     - Doubles score nothing (unless a 1 or a 5) as above
+#     - All scoring dice must be rolled in a single turn (i.e. they
+#     are not additive over turns)
+#     - Rolling all 6 will be two sets and scored accordingly
+#         [4] + [2]
+#         [3] + [3]
+#         [2] + [2] + [2]
+#
+# Examples:
+#     1,1,1,5,5,5 ==> 1500 (1000 for 1s + 250 for 3 5s)
+#     1,1,1,1,6,6 ==> 2000 (2000 for 4 1s)
+#     5,3,6,5,3,3 ==> 400 (300 for 3 3s + 100 for 2 5s)
+#     1,2,2,3,3,5 ==> 150 (100 for a 1 and 50 for a 5)
 
 
-
-#kanske def rounds(players):
+# kanske def rounds(players):
